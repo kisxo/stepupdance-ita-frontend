0fb6220499bd50c579cc3fda1330e39e11417ed9
uday <udaysubba2004@gmail.com>
2025-01-28 22:29:47 +0530

    add landing page hero section

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,55 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+    import EventCards from "./EventCards.svelte";
+</script>
+
+<div class="hero">
+    <span class="hero-title">Step Up<br> And <br>Let The <br> Talents Shine!</span>
+    <span class="hero-text">Register now for Incredible Talent of Assam <br> Showcase your incredible talents at this stage <br> Win an opportunity to work with Step Up Production!</span>
+    <div class="hero-button-container">
+        <a class="hero-register" href="">Register</a>
+        <a class="hero-login" href="">Login</a>
+    </div>
+</div>
+
+<style>
+    .hero{
+        margin-block: 1rem;
+        padding: 1rem;
+        display: flex;
+        flex-direction: column;
+        gap: 1rem;
+        text-align: center;
+    }
+
+    .hero-title{
+        font-size: 1.8rem;
+        font-weight: bold;
+    }
+
+    .hero-text{
+        font-size: .8rem;
+        padding-block: .5rem;
+    }
+
+    .hero-button-container{
+        display: flex;
+        flex-direction: row;
+        justify-content: center;
+        gap: 15px;
+
+        & > a {
+            width: 30%;
+            padding-block: .5rem;
+            border-radius: .2rem;
+        }
+
+        .hero-register{
+            background-color: black;
+            color: white;
+        }
+
+        .hero-login{
+            outline: 1px solid #666666;
+        }
+    }
+</style>
