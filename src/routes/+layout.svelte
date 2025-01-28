7137f680308d4c6234f2950be11e80506595eb72
uday <udaysubba2004@gmail.com>
2025-01-28 22:29:11 +0530

    add simple navbar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,8 @@
 <script>
 	import '../app.css';
 	let { children } = $props();
+	import Navbar from './Navbar.svelte';
 </script>
 
+<Navbar/>
 {@render children()}
---
+++ src/routes/Navbar.svelte
@@ -0,0 +1,22 @@
+<div class="nav-container box-shadow">
+    <a class="nav-title" href="/">Incredible Talent of Assam</a>
+
+    <a class="auth-links" href="#">Login</a>
+</div>
+
+<style>
+    .nav-container{
+        padding: 1rem;
+        display: flex;
+    }
+
+    .nav-title{
+        font-weight: bold;
+        margin-right: auto;
+    }
+
+    .box-shadow {
+        box-shadow: 0 2px 8px rgb(0 0 0 / 0.1);
+    }
+
+</style>
