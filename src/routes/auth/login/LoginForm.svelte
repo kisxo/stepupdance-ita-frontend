0cfa6b4bc5c59d1795ced76a9a15b2bef5d047d8
uday <udaysubba2004@gmail.com>
2025-01-29 10:04:45 +0530

    move login under auth route

--- src/routes/auth/login/LoginForm.svelte
+++ src/routes/auth/login/LoginForm.svelte
@@ -2,7 +2,6 @@
     const url = "http://localhost:8000";
     let phone = $state();
     let password = $state();
-    import {user} from "../../lib/user.svelte.js";
 
     function login() {
         console.log(user)
