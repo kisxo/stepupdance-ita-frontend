267aec1f5b738d97b5b7fcf031fd6d0601ccbbdd
uday <udaysubba2004@gmail.com>
2025-01-31 19:31:13 +0530

    add event cards

---
+++ src/routes/register/EventCard.svelte
@@ -0,0 +1,31 @@
+<div class="event-card-container">
+    <div class="event-card">
+        <span class="event-title">
+            Dance
+        </span>
+    </div>
+    <div class="event-card">
+        <span class="event-title">
+            Fashion
+        </span>
+    </div>
+</div>
+
+<style>
+    .event-card-container{
+        margin-block: 2rem;
+        padding-block: 2rem;
+        background-color: blue;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        gap: 2rem;
+    }
+
+    .event-card{
+        background-color: coral;
+        border-radius: 1rem;
+        min-height: 10rem;
+        width: 80%;
+    }
+</style>
---
+++ src/routes/register/EventCardsList.svelte
@@ -0,0 +1,10 @@
+<script>
+    import EventCard from "./EventCard.svelte";
+
+    let n = [1,2,3,4]
+</script>
+
+{#each n as m}
+    <EventCard/>
+{/each}
+
