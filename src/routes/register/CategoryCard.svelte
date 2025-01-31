798ca4245b569598ae939164ee27184a2d9db1ac
uday <udaysubba2004@gmail.com>
2025-02-01 02:50:27 +0530

    update category page

--- src/routes/register/CategoryCard.svelte
+++ src/routes/register/CategoryCard.svelte
@@ -1,4 +1,7 @@
 <div class="category-container">
+    <span class="category-title">
+        Choose a Category
+    </span>
     <div class="category-card">
         <div class="card-image-container">
             <img src="/images/hiphop.png" alt="">
@@ -7,61 +10,98 @@
             <span class="card-title"> Dance </span>
         </div>
     </div>
+
     <div class="category-card">
-        <span class="card-title"> Fashion </span>
+        <div class="card-detail">
+            <span class="card-title"> Fashion </span>
+        </div>
+        <div class="card-image-container">
+            <img src="/images/fashion-miss.png" alt="">
+        </div>
     </div>
+
     <div class="category-card">
-        <span class="card-title"> Art </span>
+        <div class="card-image-container">
+            <img src="/images/art.png" alt="">
+        </div>
+        <div class="card-detail">
+            <span class="card-title"> Art </span>
+        </div>
     </div>
+
     <div class="category-card">
-        <span class="card-title"> Yoga </span>
+        <div class="card-detail">
+            <span class="card-title"> Cooking </span>
+        </div>
+        <div class="card-image-container">
+            <img src="/images/cooking.png" alt="">
+        </div>
     </div>
+
     <div class="category-card">
-        <span class="card-title"> Cooking </span>
+        <div class="card-image-container">
+            <img src="/images/yoga.png" alt="">
+        </div>
+        <div class="card-detail">
+            <span class="card-title"> Yoga </span>
+        </div>
     </div>
 </div>
 
 <style>
     .category-container {
-        outline: 1px solid grey;
         display: flex;
         flex-direction: column;
         padding: 1rem;
         gap: 1.5rem;
     }
 
+    .category-title{
+        color: var(--primary-500);
+        text-align: center;
+        font-weight: bold;
+        font-size: 2.3rem;
+    }
+
     .category-card {
-        /* background-color: #E43D12; */
-        box-shadow: rgba(0, 0, 0, 0.2) 0px 5px 10px;
-        border-radius: 0.2rem;
+        border-radius: 0.5rem;
         padding: 1rem;
         display: flex;
         flex-direction: row;
         justify-content: space-between;
-        /* color: white; */
+        background-color: var(--primary-100);
+        color: var(--primary-400);
+        border-top: 1px solid var(--accent-500);
+        border-right: 1px solid var(--accent-500);
+        border-left: 5px solid var(--primary-500);
+        border-bottom: 5px solid var(--primary-500);
     }
 
     .card-image-container{
         width: 40%;
         border-radius: .3rem;
-        border-left: 4px solid rgb(228, 87, 87);
-        border-bottom: 4px solid rgb(231, 74, 74);
-
+        height: 9rem;
+        display: flex;
+        justify-content: center;
+        align-items: center;
         & > img{
-            filter: drop-shadow(-7px 7px 4px #333333);
+            max-height: 100%;
+            filter: drop-shadow(-2px 2px 8px #666666);
         }
 
     }
 
     .card-detail{
+        /* background-color: var(--white); */
         width: 50%;
         display: block;
         place-content: center;
+        text-align: center;
     }
 
     .card-title{
         font-weight: bold;
-        font-size: 3rem;
+        font-size: 2.1rem;
     }
 
 </style>
