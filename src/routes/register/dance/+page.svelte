1d0b2954b8d633803a667cdc7cadd8db1c9a57e9
uday <udaysubba2004@gmail.com>
2025-02-01 22:10:42 +0530

    add auth check on register route

--- src/routes/register/dance/+page.svelte
+++ src/routes/register/dance/+page.svelte
@@ -1,1 +1,44 @@
-Dance
+<script>
+    import Cookies from "js-cookie"
+    import { page } from "$app/stores";
+    import { goto } from '$app/navigation';
+    import {api_url} from "$lib/config";
+
+    import Loader from "$lib/components/Loader.svelte";
+
+    let auth_state = $state(false);
+
+    const page_url = $page.url;
+
+    let options = {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': "Bearer " + Cookies.get("jwt")
+        }
+    };
+
+    fetch(api_url+'/users/', options)
+    .then(response => {
+        if(response.status === 401)
+        {   
+
+            console.error("Auth Failed")
+            goto('/auth/signup?redirect=' + page_url.pathname);
+        }
+        return response.json();
+    })
+    .then(data => {
+        auth_state = true
+        pass
+    })
+    .catch(error => {
+    });
+
+</script>
+
+{#if auth_state}
+pass
+{:else}
+    <Loader/>
+{/if}
