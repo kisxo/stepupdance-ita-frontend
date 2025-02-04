50792475113e1c37b2f3d0c220ab1cd1e999d907
uday <udaysubba2004@gmail.com>
2025-02-04 14:06:32 +0530

    update modal to not close on outside click

--- src/lib/components/SuccessModal.svelte
+++ src/lib/components/SuccessModal.svelte
@@ -2,10 +2,18 @@
     import { Dialog, Label, Separator } from "bits-ui";
     import { fade } from "svelte/transition";
     import { flyAndScale } from "$lib/bits-ui/utils/transitions";
-    let { toggle, body, cancel="cancel"} = $props();
+    import { goto } from "$app/navigation";
+    let { toggle, body, cancel="cancel", redirect=''} = $props();
+
+    function check_redirect(){
+      if(redirect != '')
+      {
+        goto(redirect);
+      }
+    }
 </script>
    
-  <Dialog.Root bind:open={toggle}>
+  <Dialog.Root bind:open={toggle} closeOnOutsideClick={false}>
     <Dialog.Portal>
       <Dialog.Overlay transition={fade} transitionConfig={{ duration: 150 }} class="fixed inset-0 z-50 bg-black/80"/>
       <Dialog.Content transition={flyAndScale} class="fixed text-center flex flex-col justify-center gap-[2rem] bg-white border-none rounded-md py-[3rem] left-[50%] top-[50%] z-50 w-full max-w-[94%] translate-x-[-50%] translate-y-[-50%] rounded-card-lg border bg-background p-5 shadow-popover outline-none sm:max-w-[490px] md:w-full">
@@ -19,7 +27,7 @@
             {body}
         </Dialog.Description>
         
-        <Dialog.Close class="bg-blue-500 text-white right-5 top-5 rounded-xm mx-auto w-[75%] py-[.5rem] focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-foreground focus-visible:ring-offset-2 focus-visible:ring-offset-background active:scale-98">
+        <Dialog.Close onclick={check_redirect} class="bg-blue-500 text-white right-5 top-5 rounded-xm mx-auto w-[75%] py-[.5rem] focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-foreground focus-visible:ring-offset-2 focus-visible:ring-offset-background active:scale-98">
             <span>{cancel}</span>
         </Dialog.Close>
       </Dialog.Content>
